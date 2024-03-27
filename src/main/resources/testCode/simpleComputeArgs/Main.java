7e1ed8ba6258bcbdba439191b4978757b6b3e71f
yutianzeabc <yutianzeabc@126.com>
2024-03-27 21:25:11 +0800

    Fix: 引入JsonConfig处理精度丢失的配置

--- src/main/resources/testCode/simpleComputeArgs/Main.java
+++ src/main/resources/testCode/simpleComputeArgs/Main.java
@@ -4,10 +4,9 @@
  * @date: 2024/3/1
  */
 public class Main {
-
     public static void main(String[] args) throws Exception {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        System.out.println("结果:" + (a + b));
+        System.out.println(a + b);
     }
 }
